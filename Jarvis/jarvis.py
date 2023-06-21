18b94054bf45b9787fc2c359f05c22f9ff29155b
UnboundDelta <137292378+UnboundDelta@users.noreply.github.com>
2023-06-21 17:45:57 +0530

    Initial commit

---
+++ Jarvis/jarvis.py
@@ -0,0 +1,76 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    searching = ['search for', 'search', 'google', 'look for']
+    opening = ['open this site', 'open site', 'open this website', 'open website']
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+        if 'search for' in query or 'search' in query or 'google' in query or 'look for' in query:
+            speak("I am in Searching condition")
+        elif 'open this site' in query or 'open site' in query or 'open this website' in query or 'open website' in query:
+            speak("I am in website loop")
+        elif 'quit' in query:
+            break
+        else:
+            speak('Naah I am in somewhere else now')
+        
